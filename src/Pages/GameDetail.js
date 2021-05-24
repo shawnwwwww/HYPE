2fcd720b57a77bcdc09b1478af194919f6da2f4c
shawn <shawnwwwww15@gmail.com>
2021-05-24 15:54:34 -0700

    added GameDetail.css

--- src/Pages/GameDetail.js
+++ src/Pages/GameDetail.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ActivityRankCard from '../Components/ActivityRankCard'
 import ActivityHypeListCard from '../Components/ActivityHypeListCard'
-
+import './GameDetail.css'
+import Dropdown from '../Components/Dropdown'
 
 
 const GameDetail = () => {
   return (
-    <div className="GameDetail">
-      GAME DETAIL PAGE
+    <div className="pageContainer">
+      <div className='backNavContainer'>
+        <div className='backIcon'></div>
+        <h5>BACK</h5>
+      </div>
+
+      <div className='gameDetailContainer'>
+        <div className='gameDetailCardContainer'>
+          
+        </div>
+        <div className='gameDetailSectionContainer'>
+            <h5>Purchase</h5>
+            <div className='dropdownGroup'>
+              <Dropdown isPlatform='true'/>
+              <Dropdown isPlatform=''/>
+            </div>
+
+        </div>
+      </div>
     </div>
+    
   );
 };
 
