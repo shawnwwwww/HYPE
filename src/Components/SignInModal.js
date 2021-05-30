5eed572e9c8c2e18af6781c33d35eb9100481a93
shawn <shawnwwwww15@gmail.com>
2021-05-30 15:19:43 -0700

    sign in modal finished!

--- src/Components/SignInModal.js
+++ src/Components/SignInModal.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './SignInModal.css'
+import { X } from 'react-feather';
+
 
 export const SignInModal = ({showModal, setShowModal}) => {
 
@@ -10,22 +12,29 @@ export const SignInModal = ({showModal, setShowModal}) => {
 
             <div className='signInPage'>
                 <div className='signInModalContainer'>
-                    <div className='closeIcon'>
-
-                    </div>
                     <div className='signInModalContent'>
-                        <div className='signInModalHeader'>
-                            <h4>SIGN IN</h4>
-                            <ul>
-                                <li>Add to your HYPE list.</li>
-                                <li>Customize your notification preferences.</li>
-                                <li>Receive updates for your HYPEd games.</li>
-                            </ul>
+                        <div className='signInHeaderAndCloseIcon'>
+                            <div className='signInModalHeader'>
+                                <h4>SIGN IN</h4>
+                                <ul>
+                                    <li><p>Add to your HYPE list.</p></li>
+                                    <li><p>Customize your notification preferences.</p></li>
+                                    <li><p>Receive updates for your HYPEd games.</p></li>
+                                </ul>
+                            </div>
+                            <div className='closeIcon'>
+                                {/* <img>
+                                </img> */}
+                                <X color="#DE5BFF" size={24} />
+                            </div>
+                            
                         </div>
+                        
                         <div className='signInButtonGroup'>
                             <div className='signInButton'>
-                                <img className='signInLogo' id='googleIcon'></img>
-                                <p className='buttonText'>Continue with Google</p>
+                                <img className='signInLogo' id='googleIcon'
+                                src='/Resources/logo/google.png'></img>
+                                <p className='buttonText' id='googleButtonText'>Continue with Google</p>
                             </div>
                         </div>
                     </div>
