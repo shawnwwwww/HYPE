8d1811ce94ec61a7874cbf0145979d6c26f882b8
Bryce <goldb@uci.edu>
2021-05-31 15:24:25 -0700

    Hype Button Complete

--- src/Components/HypeButton.js
+++ src/Components/HypeButton.js
@@ -1,30 +1,41 @@
 import React from 'react'
 import './HypeButton.css'
 
-// Need to pass in user's "hypedUp" boolean to tell if we need to 
-// add or subtract to the total, and communicate with backend for
-// true number of hypes
-
 class HypeButton extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            hypes: 2999
+            inFocus: 0,
+            hypes: 2999,
+            isHyped: false
         }
   
         this.handleClick = this.handleClick.bind(this);
+        this.handleAnimationEnd = this.handleAnimationEnd.bind(this);
         this.convertHypes = this.convertHypes.bind(this);
     }
 
     handleClick() {
-        this.setState({hypes: this.state.hypes + 1})   
+        this.setState({
+            inFocus: 1,
+            hypes: this.state.isHyped ? this.state.hypes - 1 : this.state.hypes + 1,
+            isHyped: !this.state.isHyped
+        })
+    }
+
+    handleAnimationEnd() {
+        this.setState({
+            inFocus: 0
+        })
     }
 
     convertHypes() {
         if (this.state.hypes > 999999) {
             return Math.floor(this.state.hypes / 100000) / 10 + 'm';
+
         } else if (this.state.hypes > 999) {
             return Math.floor(this.state.hypes / 100) / 10 + 'k';
+
         } else {
             return this.state.hypes;
         }
@@ -33,11 +44,30 @@ class HypeButton extends React.Component {
     render() {
         return (
             <div className='hypeButtonContainer'>
-                <div className='hypeButton' onClick={this.handleClick}>
-                    <p className='buttonText'>HYPE</p>
-                    <span><p className='buttonText'>{this.convertHypes()}</p></span>
+
+                <div className='hypeButton'
+                    style={{
+                        backgroundColor: this.state.isHyped ?
+                            'rgba(222, 91, 255, 0.2)' : 'rgba(222, 91, 255, 0.04)'
+                    }}
+                    onClick={this.handleClick}
+                >
+
+                    <p className='buttonText' inFocus='0'>
+                        {this.state.isHyped ? 'HYPED' : 'HYPE'}
+                    </p>
+
+                    <span inFocus={this.state.inFocus}
+                        data={this.state.isHyped ? '+1' : '-1'}
+                        onAnimationEnd={() => this.handleAnimationEnd()}
+                    >
+
+                        <p className='buttonText'> {this.convertHypes()} </p>
+
+                    </span>
+
                 </div>
-                
+
             </div>
 
         )
