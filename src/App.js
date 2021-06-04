390f039dcfd9db004f3999191945741daede028c
shawn <shawnwwwww15@gmail.com>
2021-06-04 06:52:54 -0700

    adjusted routing for profile page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Components/Sidebar'
 import GameItem from './Components/GameItem'
-import React from 'react';
-
 
 
 import {
@@ -18,16 +16,22 @@ import {
 // Pages
 import Home from "./Pages/Home"
 import GameDetail from "./Pages/GameDetail"
+import Profile from './Pages/Profile'
+import AllRelease from './Pages/AllReleases'
+
+/* NOTES */
+// Right now the default page goes to "/profile" due to something with
+// history in SideBar.js I believe.
 
 function App() {
-    return (
-        <div className="App">
+  return (
+    <div className="App">
       
         <Router>
             <Switch>
                 <Route exact path="/home" component={Home} />
-                <Route exact path="/allreleases" component={Home} />
-                <Route exact path="/profile" component={GameDetail} />
+                <Route exact path="/allreleases" component={AllRelease} />
+                <Route exact path="/profile" component={Profile} />
                 <Route exact path="/gamedetail" component={GameDetail} />
                 {/*Default page if the user manually changes path*/}
                 <Route component={Home} />
@@ -37,8 +41,8 @@ function App() {
             <Sidebar />
         </Router>
       
-        </div>
-    );
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Profile.js
@@ -0,0 +1,69 @@
+import React from 'react';
+// import ProfileHypeItem from '../Components/ProfileHypeItem'
+import './Profile.css'
+// import Switch from '../Components/Switch'
+
+
+const Profile = () => {
+    return (
+
+        <div className='profilePageContainer'>
+            <div className='profilePageHeader'>
+                <div className='profileHeader'>
+
+                </div>
+                <div className='hypePreference'>
+                    <h5>HYPE Preference</h5>
+                    <div className='preferenceContainer'>
+                        <p>Send me an email reminder...</p>
+                        <ul className='preferenceSettings'>
+                            <li>
+                                <p>On release day</p>
+                                {/* <Switch /> */}
+                            </li>
+                            <li>
+                                <p>One week before release</p>
+                            </li>
+
+                        </ul>           
+                    </div>
+                </div>
+            </div>
+            <div className='profileHypeList'>
+                <h5>Your HYPE List</h5>
+                <div className='profileHypeItemGroup'>
+                    {/* grid */}
+                    {/* <ProfileHypeItem 
+                        imageLink=''
+                        title='Miitopia'
+                        releaseDate='5/21/2021'
+                    />
+                    <ProfileHypeItem 
+                        imageLink=''
+                        title='Miitopia'
+                        releaseDate='5/21/2021'
+                    />
+                    <ProfileHypeItem 
+                        imageLink=''
+                        title='Miitopia'
+                        releaseDate='5/21/2021'
+                    />
+                    <ProfileHypeItem 
+                        imageLink=''
+                        title='Miitopia'
+                        releaseDate='5/21/2021'
+                    />
+                    <ProfileHypeItem 
+                        imageLink=''
+                        title='Miitopia'
+                        releaseDate='5/21/2021'
+                    /> */}
+                </div>
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Profile;
