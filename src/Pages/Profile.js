b813b1c7d5cf34d3483ce649f4cb61d257d8214e
shawn <shawnwwwww15@gmail.com>
2021-06-04 08:19:50 -0700

    app name in index.html, favicon, profile page for desktop donw

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -10,12 +10,13 @@ const Profile = () => {
         <div className='profilePageContainer'>
             <div className='profilePageHeader'>
                 <div className='profileHeader'>
-
+                    <img id='userAvatar' src='https://i.pinimg.com/564x/e5/4f/f1/e54ff1eafb5340f6c2ac57a720f4180f.jpg'></img>
+                    <h1>Nathan</h1>
                 </div>
                 <div className='hypePreference'>
                     <h5>HYPE Preference</h5>
                     <div className='preferenceContainer'>
-                        <p>Send me an email reminder...</p>
+                        <p id='header'>Send me an email reminder...</p>
                         <ul className='preferenceSettings'>
                             <li>
                                 <p>On release day</p>
@@ -23,6 +24,19 @@ const Profile = () => {
                             </li>
                             <li>
                                 <p>One week before release</p>
+                                <Switch />
+                            </li>
+                            <li>
+                                <p>One week before release</p>
+                                <Switch />
+                            </li>
+                            <li>
+                                <p>One week before release</p>
+                                <Switch />
+                            </li>
+                            <li>
+                                <p>One week before release</p>
+                                <Switch />
                             </li>
 
                         </ul>           
@@ -34,27 +48,27 @@ const Profile = () => {
                 <div className='profileHypeItemGroup'>
                     {/* grid */}
                     <ProfileHypeItem 
-                        imageLink=''
+                        imageLink='https://upload.wikimedia.org/wikipedia/en/7/77/Miitopia_3DS.jpg'
                         title='Miitopia'
                         releaseDate='5/21/2021'
                     />
                     <ProfileHypeItem 
-                        imageLink=''
+                        imageLink='https://upload.wikimedia.org/wikipedia/en/7/77/Miitopia_3DS.jpg'
                         title='Miitopia'
                         releaseDate='5/21/2021'
                     />
                     <ProfileHypeItem 
-                        imageLink=''
+                        imageLink='https://upload.wikimedia.org/wikipedia/en/7/77/Miitopia_3DS.jpg'
                         title='Miitopia'
                         releaseDate='5/21/2021'
                     />
                     <ProfileHypeItem 
-                        imageLink=''
+                        imageLink='https://upload.wikimedia.org/wikipedia/en/7/77/Miitopia_3DS.jpg'
                         title='Miitopia'
                         releaseDate='5/21/2021'
                     />
                     <ProfileHypeItem 
-                        imageLink=''
+                        imageLink='https://upload.wikimedia.org/wikipedia/en/7/77/Miitopia_3DS.jpg'
                         title='Miitopia'
                         releaseDate='5/21/2021'
                     />
