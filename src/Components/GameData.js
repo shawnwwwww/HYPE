b324cdd6cc55ff1b8ec239203f563e38cf811050
shawn <shawnwwwww15@gmail.com>
2021-06-04 18:00:47 -0700

    used local images for rachet and clank

--- src/Components/GameData.js
+++ src/Components/GameData.js
@@ -24,56 +24,63 @@ export const GameData = [
         releaseDate: '6/11/2021', 
         platform: ["PS5", "PS4"],
         digitalImage: 'https://gmedia.playstation.com/is/image/SIEPDC/ratchet-and-clank-rift-apart-store-art-01-31mar21$en?$native$', 
-        physicalImage: 'https://previews.dropbox.com/p/thumb/ABLjebfoasrdMFP41DFULIs9D8ICwSMZwWwaD3Y4drD9DRhFJ2dCfr2goqSVmnlICGr9T9ScXYT5H2HpHi1F1AVkXtZScZUkJU6WsfRXnCCSWOwOv0EdUMMpAIpJxhAz1dr_VmjAY5IwSt6UQR8NWKE_0Jrw-ZgefJNKyf-XDlMDEmaAPhWltzu_QjLmElcw1fA4svXT7-KPKUbXY1yTIgEMWKtsfU9ORZnNYagg7rnenwHd0dpRH-e7f3lvHQu53nvz0S3VmpUq2But4RldLsH8Njm0qtcQ0jnuwjffHUO3P4DZ56Yj03lothXS9HwTbkN-ECGoDc3U9682eDTbjWgMEKK_hY1C8eTBtaKtcMdeEc80UZ4vWc4e-xNHJLsBeTv4__28ETlWSBLgSnHHHDuJA6ONMYBJstPUkVrRaJNDeA/p.png?fv_content=true&size_mode=5',
-
-    },
-    {
+        physicalImage: '/Resources/images/riftapart.png',
+        
+    },    {
         title: "Ratchet and Clank: Rift Apart", 
         releaseDate: '6/11/2021', 
         platform: ["PS5", "PS4"],
         digitalImage: 'https://gmedia.playstation.com/is/image/SIEPDC/ratchet-and-clank-rift-apart-store-art-01-31mar21$en?$native$', 
-        physicalImage: 'https://previews.dropbox.com/p/thumb/ABLjebfoasrdMFP41DFULIs9D8ICwSMZwWwaD3Y4drD9DRhFJ2dCfr2goqSVmnlICGr9T9ScXYT5H2HpHi1F1AVkXtZScZUkJU6WsfRXnCCSWOwOv0EdUMMpAIpJxhAz1dr_VmjAY5IwSt6UQR8NWKE_0Jrw-ZgefJNKyf-XDlMDEmaAPhWltzu_QjLmElcw1fA4svXT7-KPKUbXY1yTIgEMWKtsfU9ORZnNYagg7rnenwHd0dpRH-e7f3lvHQu53nvz0S3VmpUq2But4RldLsH8Njm0qtcQ0jnuwjffHUO3P4DZ56Yj03lothXS9HwTbkN-ECGoDc3U9682eDTbjWgMEKK_hY1C8eTBtaKtcMdeEc80UZ4vWc4e-xNHJLsBeTv4__28ETlWSBLgSnHHHDuJA6ONMYBJstPUkVrRaJNDeA/p.png?fv_content=true&size_mode=5',
-
-    },
-    {
+        physicalImage: '/Resources/images/riftapart.png',
+        
+    },    {
         title: "Ratchet and Clank: Rift Apart", 
         releaseDate: '6/11/2021', 
         platform: ["PS5", "PS4"],
         digitalImage: 'https://gmedia.playstation.com/is/image/SIEPDC/ratchet-and-clank-rift-apart-store-art-01-31mar21$en?$native$', 
-        physicalImage: 'https://previews.dropbox.com/p/thumb/ABLjebfoasrdMFP41DFULIs9D8ICwSMZwWwaD3Y4drD9DRhFJ2dCfr2goqSVmnlICGr9T9ScXYT5H2HpHi1F1AVkXtZScZUkJU6WsfRXnCCSWOwOv0EdUMMpAIpJxhAz1dr_VmjAY5IwSt6UQR8NWKE_0Jrw-ZgefJNKyf-XDlMDEmaAPhWltzu_QjLmElcw1fA4svXT7-KPKUbXY1yTIgEMWKtsfU9ORZnNYagg7rnenwHd0dpRH-e7f3lvHQu53nvz0S3VmpUq2But4RldLsH8Njm0qtcQ0jnuwjffHUO3P4DZ56Yj03lothXS9HwTbkN-ECGoDc3U9682eDTbjWgMEKK_hY1C8eTBtaKtcMdeEc80UZ4vWc4e-xNHJLsBeTv4__28ETlWSBLgSnHHHDuJA6ONMYBJstPUkVrRaJNDeA/p.png?fv_content=true&size_mode=5',
-
-    },
-    {
+        physicalImage: '/Resources/images/riftapart.png',
+        
+    },    {
         title: "Ratchet and Clank: Rift Apart", 
         releaseDate: '6/11/2021', 
         platform: ["PS5", "PS4"],
         digitalImage: 'https://gmedia.playstation.com/is/image/SIEPDC/ratchet-and-clank-rift-apart-store-art-01-31mar21$en?$native$', 
-        physicalImage: 'https://previews.dropbox.com/p/thumb/ABLjebfoasrdMFP41DFULIs9D8ICwSMZwWwaD3Y4drD9DRhFJ2dCfr2goqSVmnlICGr9T9ScXYT5H2HpHi1F1AVkXtZScZUkJU6WsfRXnCCSWOwOv0EdUMMpAIpJxhAz1dr_VmjAY5IwSt6UQR8NWKE_0Jrw-ZgefJNKyf-XDlMDEmaAPhWltzu_QjLmElcw1fA4svXT7-KPKUbXY1yTIgEMWKtsfU9ORZnNYagg7rnenwHd0dpRH-e7f3lvHQu53nvz0S3VmpUq2But4RldLsH8Njm0qtcQ0jnuwjffHUO3P4DZ56Yj03lothXS9HwTbkN-ECGoDc3U9682eDTbjWgMEKK_hY1C8eTBtaKtcMdeEc80UZ4vWc4e-xNHJLsBeTv4__28ETlWSBLgSnHHHDuJA6ONMYBJstPUkVrRaJNDeA/p.png?fv_content=true&size_mode=5',
-
-    },
-    {
+        physicalImage: '/Resources/images/riftapart.png',
+        
+    },    {
         title: "Ratchet and Clank: Rift Apart", 
         releaseDate: '6/11/2021', 
         platform: ["PS5", "PS4"],
         digitalImage: 'https://gmedia.playstation.com/is/image/SIEPDC/ratchet-and-clank-rift-apart-store-art-01-31mar21$en?$native$', 
-        physicalImage: 'https://previews.dropbox.com/p/thumb/ABLjebfoasrdMFP41DFULIs9D8ICwSMZwWwaD3Y4drD9DRhFJ2dCfr2goqSVmnlICGr9T9ScXYT5H2HpHi1F1AVkXtZScZUkJU6WsfRXnCCSWOwOv0EdUMMpAIpJxhAz1dr_VmjAY5IwSt6UQR8NWKE_0Jrw-ZgefJNKyf-XDlMDEmaAPhWltzu_QjLmElcw1fA4svXT7-KPKUbXY1yTIgEMWKtsfU9ORZnNYagg7rnenwHd0dpRH-e7f3lvHQu53nvz0S3VmpUq2But4RldLsH8Njm0qtcQ0jnuwjffHUO3P4DZ56Yj03lothXS9HwTbkN-ECGoDc3U9682eDTbjWgMEKK_hY1C8eTBtaKtcMdeEc80UZ4vWc4e-xNHJLsBeTv4__28ETlWSBLgSnHHHDuJA6ONMYBJstPUkVrRaJNDeA/p.png?fv_content=true&size_mode=5',
-
-    },
-    {
+        physicalImage: '/Resources/images/riftapart.png',
+        
+    },    {
         title: "Ratchet and Clank: Rift Apart", 
         releaseDate: '6/11/2021', 
         platform: ["PS5", "PS4"],
         digitalImage: 'https://gmedia.playstation.com/is/image/SIEPDC/ratchet-and-clank-rift-apart-store-art-01-31mar21$en?$native$', 
-        physicalImage: 'https://previews.dropbox.com/p/thumb/ABLjebfoasrdMFP41DFULIs9D8ICwSMZwWwaD3Y4drD9DRhFJ2dCfr2goqSVmnlICGr9T9ScXYT5H2HpHi1F1AVkXtZScZUkJU6WsfRXnCCSWOwOv0EdUMMpAIpJxhAz1dr_VmjAY5IwSt6UQR8NWKE_0Jrw-ZgefJNKyf-XDlMDEmaAPhWltzu_QjLmElcw1fA4svXT7-KPKUbXY1yTIgEMWKtsfU9ORZnNYagg7rnenwHd0dpRH-e7f3lvHQu53nvz0S3VmpUq2But4RldLsH8Njm0qtcQ0jnuwjffHUO3P4DZ56Yj03lothXS9HwTbkN-ECGoDc3U9682eDTbjWgMEKK_hY1C8eTBtaKtcMdeEc80UZ4vWc4e-xNHJLsBeTv4__28ETlWSBLgSnHHHDuJA6ONMYBJstPUkVrRaJNDeA/p.png?fv_content=true&size_mode=5',
-
-    },
-    {
+        physicalImage: '/Resources/images/riftapart.png',
+        
+    },    {
         title: "Ratchet and Clank: Rift Apart", 
         releaseDate: '6/11/2021', 
         platform: ["PS5", "PS4"],
         digitalImage: 'https://gmedia.playstation.com/is/image/SIEPDC/ratchet-and-clank-rift-apart-store-art-01-31mar21$en?$native$', 
-        physicalImage: 'https://previews.dropbox.com/p/thumb/ABLjebfoasrdMFP41DFULIs9D8ICwSMZwWwaD3Y4drD9DRhFJ2dCfr2goqSVmnlICGr9T9ScXYT5H2HpHi1F1AVkXtZScZUkJU6WsfRXnCCSWOwOv0EdUMMpAIpJxhAz1dr_VmjAY5IwSt6UQR8NWKE_0Jrw-ZgefJNKyf-XDlMDEmaAPhWltzu_QjLmElcw1fA4svXT7-KPKUbXY1yTIgEMWKtsfU9ORZnNYagg7rnenwHd0dpRH-e7f3lvHQu53nvz0S3VmpUq2But4RldLsH8Njm0qtcQ0jnuwjffHUO3P4DZ56Yj03lothXS9HwTbkN-ECGoDc3U9682eDTbjWgMEKK_hY1C8eTBtaKtcMdeEc80UZ4vWc4e-xNHJLsBeTv4__28ETlWSBLgSnHHHDuJA6ONMYBJstPUkVrRaJNDeA/p.png?fv_content=true&size_mode=5',
-
+        physicalImage: '/Resources/images/riftapart.png',
+        
+    },    {
+        title: "Ratchet and Clank: Rift Apart", 
+        releaseDate: '6/11/2021', 
+        platform: ["PS5", "PS4"],
+        digitalImage: 'https://gmedia.playstation.com/is/image/SIEPDC/ratchet-and-clank-rift-apart-store-art-01-31mar21$en?$native$', 
+        physicalImage: '/Resources/images/riftapart.png',
+        
+    },    {
+        title: "Ratchet and Clank: Rift Apart", 
+        releaseDate: '6/11/2021', 
+        platform: ["PS5", "PS4"],
+        digitalImage: 'https://gmedia.playstation.com/is/image/SIEPDC/ratchet-and-clank-rift-apart-store-art-01-31mar21$en?$native$', 
+        physicalImage: '/Resources/images/riftapart.png',
+        
     },
-
 ]
