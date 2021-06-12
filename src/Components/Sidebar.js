9524cf6f7b82bd2c58d78071119df1a3a08a0371
Justin Lock <34179707+jjlock@users.noreply.github.com>
2021-06-12 16:23:01 -0700

    Prompt user to sign in when accessing their profile while signed out

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -3,18 +3,31 @@ import './Sidebar.css';
 import { SidebarData } from './SidebarData';
 import { useHistory } from 'react-router-dom';
 import { SignInModal } from './SignInModal';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { ReactComponent as HomeIcon } from './icons/home.svg';
 import { ReactComponent as AllReleasesIcon } from './icons/allreleases.svg';
 import { ReactComponent as ProfileIcon } from './icons/profile.svg';
+import { useAuth } from '../Contexts/AuthContext';
 
 
 
 function Sidebar() {
-  const [showModal, setShowModal] = useState(false)
+  const [showModalProfile, setShowModalProfile] = useState(false);
+  const [showModalSignIn, setShowModalSignIn] = useState(false);
+  const { currentUser } = useAuth();
 
-  const openModal = () => {
-    setShowModal(prev => !prev);
+  const openModalProfile = e => {
+    if (currentUser === null) {
+      e.preventDefault();
+      setShowModalProfile(prev => !prev);
+    }
+  };
+
+  const openModalSignIn = e => {
+    if (currentUser === null) {
+      e.preventDefault();
+      setShowModalSignIn(prev => !prev);
+    }
   };
 
   const history = useHistory();
@@ -50,19 +63,20 @@ function Sidebar() {
                       </NavLink>
                     </li>
                     <li>
-                      <NavLink to="/profile" className='row' activeClassName="selected">
+                      <NavLink to="/profile" className='row' activeClassName="selected" onClick={openModalProfile}>
                         <ProfileIcon id='icon' stroke='#DE5BFF'/>
                         <h6 id='title'>Profile</h6>
                       </NavLink>
                     </li>
                 </ul>
+                <SignInModal showModal={showModalProfile} setShowModal={setShowModalProfile} />
             </div>
             
-            <div className='UserProfile'>
+            <Link to="/profile" className='UserProfile' onClick={openModalSignIn}>
                 <img id='userAvatar' src='https://i.pinimg.com/564x/e5/4f/f1/e54ff1eafb5340f6c2ac57a720f4180f.jpg' alt="User Avatar"></img>
-                <h6 onClick={openModal}>Sign In</h6>
-                <SignInModal showModal={showModal} setShowModal={setShowModal}/>
-            </div>
+                <h6>{currentUser ? currentUser.displayName : 'Sign In'}</h6>
+            </Link>
+            <SignInModal showModal={showModalSignIn} setShowModal={setShowModalSignIn}/>
             
         </div>
     );
